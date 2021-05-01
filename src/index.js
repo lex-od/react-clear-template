a7b15ac66617270cd6e0f88268db38120de8ccb9
Aleksey Yesin <esinru@gmail.com>
2021-05-01 15:55:56 +0300

    Changes file structre, adds reselect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'modern-normalize/modern-normalize.css';
-import './styles/global.scss';
+import './styles/index.scss';
+import App from './components/App';
 
 ReactDOM.render(
     <React.StrictMode>
